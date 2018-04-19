bd754977ec1e83a4aac1c31b72030c826eb46e9d
Anil Patel <ajpatel92@icloud.com>
2018-04-19 15:32:31 -0500

    game updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 
-var userGuess = null;
-var wordChoices = ["russel", "baez", "heyward", "rizzo", "contreras", "bryant", "almora", "schwarber", "fowler", "hendricks", "arrieta", "zobrist", "la stella", "fowler", "jackson", "lester", "montgomery", "edwards", "rondon", "strop", "montero", "grimm", "wood", "chapman", "lackey"];
-var indexComputerWord = null;
+var userGuess;
+var wordChoices = ["russel", "baez", "heyward", "rizzo", "contreras", "bryant", "almora", "schwarber", "fowler", "hendricks", "arrieta", "zobrist", "fowler", "jackson", "lester", "montgomery", "edwards", "rondon", "strop", "montero", "grimm", "wood", "chapman", "lackey"];
+var indexCurrentWord;
 var currentWord = [];
-var underscoreLength = 0;
+var underscoreLength = [];
 
 
 
@@ -12,24 +12,36 @@ var underscoreLength = 0;
 // generate a random guess by the computer using a function from the wordChoices array
 function generateComputerGuess(){
     // function to generate random word from array
-    computerWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-    console.log(computerWord);
-    // ALSO, get the index of the computerWord that was randomly selected and save it to a variable
-    indexComputerWord = wordChoices.indexOf(computerWord);
+    currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+    console.log(currentWord);
+    // ALSO, get the index of the currentWord that was randomly selected and save it to a variable
+    indexCurrentWord = wordChoices.indexOf(currentWord);
     // console.log the index of the word selected to make sure it worked
-    console.log(indexComputerWord);
+    // console.log(indexComputerWord);
     // get the length of the string so you can use the _'s
-    underscoreLength = wordChoices[indexComputerWord].length;
+    underscoreLength = wordChoices[indexCurrentWord].length;
     // console.log to see the length
-    console.log(underscoreLength);
+    // console.log(underscoreLength);
     // print the number of characters as "_" to the html 
     for(i = 0; i < underscoreLength; i++){
-        console.log([i]);
+        // console.log([i]);
     // append the underscores underscoreLength amount times
-       $("#currentWord").append(" _ ")[i];
+       $("#currentWord").append("_")[i];
     }
 }
 
+
+// check the guess to the currentWord string
+function checkGuess (userGuess) {
+    for( i = 0; i < currentWord.length; i++){
+        if (currentWord[i] === userGuess) {
+            console.log("hit");
+            guessPosition = currentWord.indexOf(userGuess);
+            console.log(guessPosition);
+            // underscoreLength.splice(guessPosition, userGuess);
+        } 
+    }
+}
 
 
 // call the function to begin the game and get a computer guess
@@ -40,6 +52,9 @@ document.onkeyup = function (event) {
     userGuess = event.key;
     // logged the userGuess to make sure it is working
     console.log(userGuess);
+    checkGuess(userGuess);
+
 }
+
 
 
